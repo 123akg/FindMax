f3c7bf38e8bf178aff57ec34e46dbe6800b02fd1
123akg <abhijeetkumargiri7070@gmail.com>
2020-10-05 01:35:31 +0530

    Max of 3 Strings

--- src/test/java/com/findMax/MaxTest.java
+++ src/test/java/com/findMax/MaxTest.java
@@ -8,37 +8,50 @@ import junit.framework.Assert;
 public class MaxTest {
 	
 	Max max;
-
+	
 	@Before
 	public void initialize() {
 		max = new Max();
 	}
 
-	@Test
+//INTEGER TEST	@Test
 	public void givenMaxNumber_At1stPosition_ReturnNumber() {
-		Assert.assertEquals(new Integer(6), max.findMaximumInteger(new Integer[] {4,3,6}));		
+		Assert.assertEquals(new Integer(4), max.findMaximumInteger(new Integer[] {4,2,1}));		
 	}	
 	@Test
 	public void givenMaxNumber_At2ndPosition_ReturnNumber() {
-		Assert.assertEquals(new Integer(7), max.findMaximumInteger(new Integer[] {7,2,4}));		
+		Assert.assertEquals(new Integer(7), max.findMaximumInteger(new Integer[] {3,4,7}));		
 	}
 	@Test
 	public void givenMaxNumber_At3rdPosition_ReturnNumber() {
-		Assert.assertEquals(new Integer(9), max.findMaximumInteger(new Integer[] {3,9,5}));		
+		Assert.assertEquals(new Integer(5), max.findMaximumInteger(new Integer[] {3,4,5}));		
 	}
 	
-	
+//FLOAT TEST	
 	@Test
 	public void givenMaxFloat_At1stPosition_ReturnFloat() {
-		Assert.assertEquals(new Float(2.99f), max.findMaximumFloat(new Float[] {2.8f,2.9f,2.99f}));		
+		Assert.assertEquals(new Float(2.9f), max.findMaximumFloat(new Float[] {2.9f,2f,1.8f}));		
 	}	
 	@Test
 	public void givenMaxFloat_At2ndPosition_ReturnFloat() {
-		Assert.assertEquals(new Float(6.8f), max.findMaximumFloat(new Float[] {6.79f,6.78f,6.8f}));		
+		Assert.assertEquals(new Float(3.7f), max.findMaximumFloat(new Float[] {3.7f,2.5f,1f}));		
 	}
 	@Test
 	public void givenMaxFloat_At3Position_ReturnFloat() {
-		Assert.assertEquals(new Float(7.92f), max.findMaximumFloat(new Float[] {7.9f,7.92f,7.91f}));		
+		Assert.assertEquals(new Float(5.2f), max.findMaximumFloat(new Float[] {3f,4.7f,5.2f}));		
 	}
 
+//STRING TEST
+	@Test
+	public void givenMaxString_At1stPosition_ReturnString() {
+		Assert.assertEquals(new String("Cherries"), max.findMaximumString(new String[] {"Cherries","Banana","Apple"}));		
+	}	
+	@Test
+	public void givenMaxString_At2ndPosition_ReturnString() {
+		Assert.assertEquals(new String("Grapefruit"), max.findMaximumString(new String[] {"Cherries","Grapefruit","Apple"}));		
+	}
+	@Test
+	public void givenMaxString_At3rdPosition_ReturnString() {
+		Assert.assertEquals(new String("Kiwi"), max.findMaximumString(new String[] {"Cherries","Banana","Kiwi"}));		
+	}
 }
