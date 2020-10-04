de38759de5fa23f7fab16e37b250312144137bcf
123akg <abhijeetkumargiri7070@gmail.com>
2020-10-05 01:22:38 +0530

    Max of 3 numbers

--- src/test/java/com/findMax/MaxTest.java
+++ src/test/java/com/findMax/MaxTest.java
@@ -6,5 +6,29 @@ import org.junit.Test;
 import junit.framework.Assert;
  
 public class MaxTest {
+	
+	Max max;
+
+	@Before
+	public void initialize() {
+		max = new Max();
+	}
+
+	@Test
+	public void givenMaxNumber_At1stPosition_ReturnNumber() {
+		Assert.assertEquals(new Integer(4), max.findMaximumInteger(new Integer[] {3,4,1}));
+		
+	}	
+	@Test
+	public void givenMaxNumber_At2ndPosition_ReturnNumber() {
+		Assert.assertEquals(new Integer(5), max.findMaximumInteger(new Integer[] {5,2,4}));
+		
+	}
+	@Test
+	public void givenMaxNumber_At3rdPosition_ReturnNumber() {
+		Assert.assertEquals(new Integer(9), max.findMaximumInteger(new Integer[] {6,2,9}));
+		
+	}
+
 
 }
